6ab0f1efacb139da2161da88250f9e35752a4477
Paul Stodolak <paul@jacknifedesign.com>
2017-10-12 14:24:07 -0400

    Adding open/close controls to Accordion.

--- accordion/src/js/objects/Accordion.js
+++ accordion/src/js/objects/Accordion.js
@@ -3,17 +3,28 @@ var JACKNIFE = JACKNIFE || {};
 (function( $ ) {
 	var Accordion = JACKNIFE.Accordion = function Accordion(el) {
 		// Elements
-		var $el      = el;
-		var $trigger = $('[data-acc-trigger]', $el);
+		var $el = el;
 
 		// Variables
+		var id = ($el.attr('id')) ? $el.attr('id') : null;
 
 		// Public Functions
 		this.init = function() {
+			console.log(id);
 			// Apply listeners
-			$trigger.on('click', _toggle);
+			
 			$(window).on('acc-all-open', _open);
 			$(window).on('acc-all-close', _close);
+
+			if (id) {
+				$('[data-acc-open="' + id + '"]').on('click', _open);
+				$('[data-acc-close="' + id + '"]').on('click', _close);
+				$('[data-acc-toggle="' + id + '"]').on('click', _toggle);
+			}else {
+				$('[data-acc-open]', $el).on('click', _open);
+				$('[data-acc-close]', $el).on('click', _close);
+				$('[data-acc-toggle]', $el).on('click', _toggle);
+			}
 			return;
 		}
 
