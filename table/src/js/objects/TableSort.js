251101957ece1da1a0c0380a29632af5e01b649d
Paul Stodolak <paul@jacknifedesign.com>
2018-05-10 16:32:36 -0400

    Table sorting date.

--- table/src/js/objects/TableSort.js
+++ table/src/js/objects/TableSort.js
@@ -6,8 +6,10 @@
 		var $body    = $('tbody', $el);
 
 		// Variables
-		var direction = 'DESC';
-		var rows      = $('tr', $body).toArray();
+		var direction   = 'DESC';
+		var type        = 'default';
+		var date_format = ($el.data('date-format') ? $el.data('date-format').toUpperCase() : 'DMY');
+		var rows        = $('tr', $body).toArray();
 
 		// Public Functions
 
@@ -17,6 +19,7 @@
 			var $target = $('.sort-toggle.active', $el)
 			if ($('.sort-toggle.active', $el)) {
 				_set_direction($target);
+				_set_type($target);
 				_sort($target);
 			}
 		}
@@ -29,6 +32,7 @@
 			var $target = $(this);
 
 			_set_direction($target);
+			_set_type($target);
 			_sort($target);
 		}
 
@@ -47,6 +51,14 @@
 				$toggles.removeClass('active sort-desc sort-asc');
 				$target.addClass('active sort-desc');
 				direction = 'DESC';
+			}
+		}
+
+		var _set_type = function($target) {
+			if ($target.data('sort')) {
+				type = $target.data('sort');
+			}else {
+				type = 'default';
 			}
 		}
 
@@ -69,7 +81,11 @@
 				var valA = _get_value(a, index);
 				var valB = _get_value(b, index);
 
-				if ($.isNumeric(valA) && $.isNumeric(valB)) {
+				if (type === 'date') {
+					valA = _format_date(valA);
+					valB = _format_date(valB);
+					return valA - valB;
+				}else if ($.isNumeric(valA) && $.isNumeric(valB)) {
 					return valA - valB;
 				}else {
 					valA = valA.toString();
@@ -81,6 +97,65 @@
 
 		var _get_value = function(row, index) {
 			return $('td', row).eq(index).text();
+		}
+
+		var _format_date = function(str) {
+			var date;
+			var day;
+			var month;
+			var year;
+
+			str = str.toLowerCase();
+			str = str.replace(/^[^a-z0-9]*/, '');
+			str = str.replace(/[^a-z0-9]*$/, '');
+
+			if (str.length < 1) return;
+
+			str = str.replace(/[^a-z0-9]+/g, ',');
+
+			date = str.split(',');
+
+			if (date.length < 3) return;
+
+			for (var i = 0; i < 3; i++) {
+				var target = format.substr(i, 1);
+				if (target === 'D') {
+					day = date[i];
+				}else if (target === 'M') {
+					month = date[i];
+				}else if (target === 'Y') {
+					year = date[i];
+				}
+			}
+
+			day = day.replace(/^0/, '');
+			if (day < 10) day = '0' + day;
+
+			if( /[a-z]/.test(month) ) {
+				month = month.substr(0,3);
+				switch(month) {
+					case "jan" : month = String(1); break;
+					case "feb" : month = String(2); break;
+					case "mar" : month = String(3); break;
+					case "apr" : month = String(4); break;
+					case "may" : month = String(5); break;
+					case "jun" : month = String(6); break;
+					case "jul" : month = String(7); break;
+					case "aug" : month = String(8); break;
+					case "sep" : month = String(9); break;
+					case "oct" : month = String(10); break;
+					case "nov" : month = String(11); break;
+					case "dec" : month = String(12); break;
+					default    : month = String(0);
+				}
+			}
+
+			month = month.replace(/^0/, '');
+			if (month < 10) month = '0' + month;
+
+			year = parseInt(year);
+			if( year < 100 ) { year = parseInt(year) + 2000; }
+			return "" + String(year) + "" + String(month) + "" + String(day) + "";
 		}
 	}
 })( jQuery );
