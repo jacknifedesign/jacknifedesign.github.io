90e662b51f3f17aef295fa08a01ceca7ff1fbe8d
Paul Stodolak <paul@jacknifedesign.com>
2018-05-10 10:51:17 -0400

    Correcting error preventing js from initializing.

--- table/src/js/app.js
+++ table/src/js/app.js
@@ -2,12 +2,12 @@ var JACKNIFE = JACKNIFE || {};
 
 (function( $ ) {
 	$( document ).ready(function() {
-		$('table-sort').each(function() {
+		$('.table-sort').each(function() {
 			var table = new JACKNIFE.TableSort($(this));
 			table.init();
 		});
 
-		$('table-filter').each(function() {
+		$('.table-filter').each(function() {
 			var table = new JACKNIFE.TableFilter($(this));
 			table.init();
 		});
