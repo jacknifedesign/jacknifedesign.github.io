5aad71e412cc9e7d54b7f5ff206039610488242f
Paul Stodolak <paul@jacknifedesign.com>
2018-05-14 16:52:19 -0400

    Merging Table Filter and Sort.

--- table/src/js/app.js
+++ table/src/js/app.js
@@ -2,18 +2,6 @@ var JACKNIFE = JACKNIFE || {};
 
 (function( $ ) {
 	$( document ).ready(function() {
-		/*$('.table-sort').each(function() {
-			var TableSort = new JACKNIFE.TableSort($(this));
-			TableSort.init();
-			$(this).data('TableSort', TableSort);
-		});*/
-
-		/*$('.table-filter').each(function() {
-			var TableFilter = new JACKNIFE.TableFilter($(this));
-			TableFilter.init();
-			$(this).data('TableFilter', TableFilter);
-		});*/
-
 		$('.table-filter, .table-sort').each(function() {
 			var TableController = new JACKNIFE.TableController($(this));
 			TableController.init();
